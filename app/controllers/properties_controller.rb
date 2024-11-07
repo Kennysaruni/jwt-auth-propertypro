0150002924840c34207768156bd9346978dc6ddc
Kennysaruni <kennylatimore4@gmail.com>
2024-11-07 18:08:19 +0300

    Completed file upload functionality

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -5,8 +5,17 @@ class PropertiesController < ApplicationController
     end
 
     def create
+        Rails.logger.debug "Received params: #{params.inspect}"
         owner = current_user
-        property = owner.properties.create!(property_params)
+        property = owner.properties.create!(property_params.except(:images))
+        images = params[:property][:images]
+
+        if images
+            images.each do |image|
+                property.images.attach(image)
+            end
+        end
+
         if property.save
             render json: {property: PropertySerializer.new(property)}, status: :created
         else
@@ -16,15 +25,23 @@ class PropertiesController < ApplicationController
 
     def show
         property = Property.find_by(id: params[:id])
+
         if property
-            render json: {property: PropertySerializer.new(property)}, status: :ok
+            # Collect URLs of attached images
+            image_urls = property.images.map { |image| url_for(image) }
+
+            # Render property with serialized data and image URLs
+            render json: {
+                property: PropertySerializer.new(property).as_json.merge(images: image_urls)
+            }, status: :ok
         else
-            render json: {error: 'Property not found'}, status: :not_found
+            render json: { error: 'Property not found' }, status: :not_found
         end
     end
 
+
     private
     def property_params
-        params.permit(:property_name,:location)
+        params.require(:property).permit(:property_name,:location, images: [])
     end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,6 @@ class Property < ApplicationRecord
     belongs_to :owner
     has_many :units
     has_many :leases
+
+    has_many_attached :images
 end
